645d837903e45e4d093d2104b00680129f1eac73
v.vasilev <v.vasilev@hh.ru>
2024-01-05 02:53:52 +0300

    cart create admin controller covered by tests

---
+++ src/tests/unit/api/rest/admin/v1/carts/test_create.py
@@ -0,0 +1,120 @@
+from http import HTTPStatus
+from typing import Any
+from unittest.mock import AsyncMock
+
+import pytest
+from _pytest.fixtures import SubRequest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from pytest_mock import MockerFixture
+
+from app.app_layer.interfaces.auth_system.exceptions import InvalidAuthDataError, OperationForbiddenError
+from app.app_layer.interfaces.use_cases.carts.create_cart import ICreateCartUseCase
+from app.app_layer.interfaces.use_cases.carts.dto import CartOutputDTO
+from app.domain.carts.value_objects import CartStatusEnum
+from app.domain.interfaces.repositories.carts.exceptions import (
+    ActiveCartAlreadyExistsError,
+)
+from tests.utils import fake
+
+
+@pytest.fixture()
+def url_path() -> str:
+    return "api/admin/v1/carts"
+
+
+@pytest.fixture()
+def use_case(request: SubRequest, mocker: MockerFixture) -> AsyncMock:
+    mock = mocker.AsyncMock(spec=ICreateCartUseCase)
+
+    if "returns" in request.param:
+        mock.create_by_user_id.return_value = request.param["returns"]
+    elif "raises" in request.param:
+        mock.create_by_user_id.side_effect = request.param["raises"]
+
+    return mock
+
+
+@pytest.fixture()
+def application(application: FastAPI, use_case: AsyncMock) -> FastAPI:
+    with application.container.create_cart_use_case.override(use_case):
+        yield application
+
+
+@pytest.mark.parametrize(
+    "use_case",
+    [
+        {
+            "returns": CartOutputDTO(
+                created_at=fake.datetime.datetime(),
+                id=fake.cryptographic.uuid_object(),
+                user_id=fake.numeric.integer_number(start=1),
+                status=CartStatusEnum.OPENED,
+                items=[],
+                items_qty=0,
+                cost=0,
+                checkout_enabled=False,
+                coupon=None,
+            ),
+        }
+    ],
+    indirect=True,
+)
+async def test_ok(http_client: AsyncClient, use_case: AsyncMock, url_path: str) -> None:
+    response = await http_client.post(
+        url=url_path,
+        json=fake.numeric.integer_number(start=1),
+    )
+
+    assert response.status_code == HTTPStatus.OK, response.text
+    assert response.json() == {
+        "created_at": use_case.create_by_user_id.return_value.created_at.isoformat(),
+        "id": str(use_case.create_by_user_id.return_value.id),
+        "user_id": use_case.create_by_user_id.return_value.user_id,
+        "status": use_case.create_by_user_id.return_value.status.value,
+        "items": use_case.create_by_user_id.return_value.items,
+        "items_qty": float(use_case.create_by_user_id.return_value.items_qty),
+        "cost": float(use_case.create_by_user_id.return_value.cost),
+        "checkout_enabled": use_case.create_by_user_id.return_value.checkout_enabled,
+        "coupon": use_case.create_by_user_id.return_value.coupon,
+    }
+
+
+@pytest.mark.parametrize(
+    ("use_case", "expected_code", "expected_error"),
+    [
+        pytest.param(
+            {"raises": InvalidAuthDataError},
+            HTTPStatus.UNAUTHORIZED,
+            {"detail": {"code": 1000, "message": "Authorization failed."}},
+            id="UNAUTHORIZED",
+        ),
+        pytest.param(
+            {"raises": ActiveCartAlreadyExistsError},
+            HTTPStatus.BAD_REQUEST,
+            {"detail": {"code": 2001, "message": "Active cart already exists."}},
+            id="ACTIVE_CART_ALREADY_EXISTS",
+        ),
+        pytest.param(
+            {"raises": OperationForbiddenError},
+            HTTPStatus.BAD_REQUEST,
+            {"detail": {"code": 1001, "message": "Forbidden."}},
+            id="FORBIDDEN",
+        ),
+    ],
+    indirect=["use_case"],
+)
+async def test_failed(
+    http_client: AsyncClient,
+    use_case: AsyncMock,
+    url_path: str,
+    expected_code: int,
+    expected_error: dict[str, Any],
+) -> None:
+    response = await http_client.post(
+        url=url_path,
+        json=fake.numeric.integer_number(start=1),
+    )
+
+    assert response.status_code == expected_code, response.text
+    assert response.json() == expected_error
