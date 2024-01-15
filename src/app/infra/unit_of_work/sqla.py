a06bab0984b4ec984cef26b6ef14f1f6ff4001e9
v.vasilev <v.vasilev@hh.ru>
2024-01-15 19:48:54 +0300

    asyncio.gather sqla session ability

--- src/app/infra/unit_of_work/sqla.py
+++ src/app/infra/unit_of_work/sqla.py
@@ -1,3 +1,5 @@
+from contextvars import ContextVar
+
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from app.app_layer.interfaces.unit_of_work.sql import IUnitOfWork
@@ -16,27 +18,43 @@ class Uow(IUnitOfWork):
     def __init__(self, session_factory: async_sessionmaker[AsyncSession]) -> None:
         self._session_factory = session_factory
 
-    async def __aenter__(self) -> IUnitOfWork:
-        self._session = self._session_factory()
+        # AsyncSession не подходит для многозадачности в асинхронных контекстах:
+        # https://docs.sqlalchemy.org/en/20/orm/session_basics.html#is-the-session-thread-safe-is-asyncsession-safe-to-share-in-concurrent-tasks
+        # Используем ContextVar, чтобы безопасно работать с одним объектом Uow
+        # в операциях типа asyncio.gather(...).
+        self._session_ctx: ContextVar[AsyncSession] = ContextVar("_sqla_session")
 
-        self.items = ItemsRepository(session=self._session)
-        self.carts = CartsRepository(session=self._session)
-        self.cart_coupons = CartCouponsRepository(session=self._session)
-        self.carts_notifications = CartsNotificationsRepository(session=self._session)
+    async def __aenter__(self) -> IUnitOfWork:
+        session = self._session_factory()
+        self._session_ctx.set(session)
+
+        self.items = ItemsRepository(session=session)
+        self.carts = CartsRepository(session=session)
+        self.cart_coupons = CartCouponsRepository(session=session)
+        self.carts_notifications = CartsNotificationsRepository(session=session)
 
         return await super().__aenter__()
 
     async def commit(self) -> None:
-        """Commits the changes made in the session."""
+        """
+        Retrieves the current session from the context variable _sqla_session and calls
+        the commit method on the session to commit the changes made in the session.
+        """
 
-        await self._session.commit()
+        await self._session_ctx.get().commit()
 
     async def rollback(self) -> None:
-        """Rolls back the changes made in the session."""
+        """
+        Retrieves the current session from the context variable _sqla_session and calls
+        the rollback method on the session to roll back the changes made in the session.
+        """
 
-        await self._session.rollback()
+        await self._session_ctx.get().rollback()
 
     async def shutdown(self) -> None:
-        """Closes the session."""
+        """
+        Retrieves the current session from the context variable _sqla_session and Call
+        the close method on the session to close it.
+        """
 
-        await self._session.close()
+        await self._session_ctx.get().close()
